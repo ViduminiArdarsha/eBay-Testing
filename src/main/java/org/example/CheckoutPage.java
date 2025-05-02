82faf384b2b4ca96181515d3e5a6058ffc60d535
ViduminiArdarsha <ghvidumini@gmail.com>
2025-05-01 12:28:31 -1200

    Checkout Error Resolved

--- src/main/java/org/example/CheckoutPage.java
+++ src/main/java/org/example/CheckoutPage.java
@@ -13,7 +13,16 @@ public class CheckoutPage {
     WebDriver driver;
     WebDriverWait wait;
 
-     guestEmailPath= By.name("firstName");
+    private final By firstNamePath = By.name("firstName");
+    private final By lastNamePath = By.name("lastName");
+    private final By addressPath1 = By.name("addressLine1");
+    private final By addressPath2 = By.name("addressLine2");
+    private final By cityPath = By.name("city");
+    private final By stateOrProvincePath = By.name("stateOrProvince");
+    private final By postalCodePath = By.name("postalCode");
+    private final By emailPath = By.name("email");
+    private final By emailConfirmPath = By.name("emailConfirm");
+    private final By phoneNumberPath = By.name("phoneNumber");
 
     public CheckoutPage(WebDriver driver) {
         this.driver = driver;
@@ -32,15 +41,19 @@ public class CheckoutPage {
 
     }
 
-    public void fillCheckoutForm(String email, String firstName, String lastName,
-                                 String address, String city, String state, String zip) {
-        wait.until(ExpectedConditions.visibilityOf(guestEmailPath)).sendKeys(email);
-        firstNameInput.sendKeys(firstName);
-        lastNameInput.sendKeys(lastName);
-        addressInput.sendKeys(address);
-        cityInput.sendKeys(city);
-        stateInput.sendKeys(state);
-        zipCodeInput.sendKeys(zip);
+    public void fillCheckoutForm(String firstName, String lastName,String address1,String address2, String city,String stateOrProvince, String postalCode,String email,String emailConfirm,String phoneNumber) {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(firstNamePath)).sendKeys(firstName);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(lastNamePath)).sendKeys(lastName);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(addressPath1)).sendKeys(address1);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(addressPath2)).sendKeys(address2);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(cityPath)).sendKeys(city);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(stateOrProvincePath)).sendKeys(stateOrProvince);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(postalCodePath)).sendKeys(postalCode);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(emailPath)).sendKeys(email);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(emailConfirmPath)).sendKeys(emailConfirm);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(phoneNumberPath)).sendKeys(phoneNumber);
+
+
 
 
     }
